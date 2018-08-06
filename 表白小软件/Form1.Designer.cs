e09b82fab6557bf47108beee1bac7102b5d31bf9
HoursX <jxjsjianghao@outlook.com>
2018-08-06 22:15:28 +0800

    使用Visual Studio开发的一款WinForm小程序

---
+++ 表白小软件/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace 表白小软件
+{
+    partial class fm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fm));
+            this.btOK = new System.Windows.Forms.Button();
+            this.btNO = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lb = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btOK
+            // 
+            this.btOK.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.btOK, "btOK");
+            this.btOK.Name = "btOK";
+            this.btOK.UseVisualStyleBackColor = false;
+            this.btOK.Click += new System.EventHandler(this.btOK_Click);
+            // 
+            // btNO
+            // 
+            resources.ApplyResources(this.btNO, "btNO");
+            this.btNO.Name = "btNO";
+            this.btNO.UseVisualStyleBackColor = true;
+            this.btNO.Click += new System.EventHandler(this.btNO_Click);
+            this.btNO.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btNO_MouseMove);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Image = global::表白小软件.Properties.Resources.TIM图片20180805202421;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // lb
+            // 
+            resources.ApplyResources(this.lb, "lb");
+            this.lb.Name = "lb";
+            this.lb.Click += new System.EventHandler(this.lb_Click);
+            // 
+            // fm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.lb);
+            this.Controls.Add(this.btNO);
+            this.Controls.Add(this.btOK);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "fm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fm_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.fm_FormClosed);
+            this.Load += new System.EventHandler(this.送给亲爱的你_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btOK;
+        private System.Windows.Forms.Button btNO;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lb;
+    }
+}
+
---
+++ 表白小软件/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 表白小软件
+{
+    public partial class fm : Form
+    {
+        private int okCount = 0;
+        private int noCount = 0;
+        private bool flag = true;
+        public fm()
+        {
+            InitializeComponent();
+        }
+
+        private void 送给亲爱的你_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (flag)
+            {
+                switch (e.CloseReason)
+                {
+                    case CloseReason.ApplicationExitCall:
+                        e.Cancel = true;
+                        break;
+                    case CloseReason.FormOwnerClosing:
+                        e.Cancel = true;
+                        break;
+                    case CloseReason.MdiFormClosing:
+                        e.Cancel = true;
+                        break;
+                    case CloseReason.None:
+                        break;
+                    case CloseReason.TaskManagerClosing:
+                        e.Cancel = false;
+                        break;
+                    case CloseReason.UserClosing:
+                        e.Cancel = true;
+                        break;
+                    case CloseReason.WindowsShutDown:
+                        e.Cancel = false;
+                        break;
+                    default:
+                        break;
+                }
+                MessageBox.Show("你不爱我可是关不掉的哦！", "警告");
+                lb.Text = "做我女朋友好吗？";
+            }
+        }
+
+        private void fm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void btNO_Click(object sender, EventArgs e)
+        {
+            flag = true;
+            if(okCount >= 1)
+            {
+                lb.Text = "说好的爱我呢,wuwuwuwuwu";
+            }
+            if (++noCount == 1)
+            {
+                lb.Text = "我要生气了";
+            }
+            if(noCount == 2)
+            {
+                lb.Text = "好的，我真的生气了";
+            }
+        }
+
+        private void btOK_Click(object sender, EventArgs e)
+        {
+            okCount++;
+            System.Diagnostics.Process.Start("cmd.exe", "/cshutdown -a");
+            lb.Text = "爱你哟!";
+            MessageBox.Show("恭喜你获得一枚男朋友！", "小惊喜");
+            flag = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lb_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btNO_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            if (noCount > 2)
+            {
+                okCount+=150;
+                noCount++;
+                btNO.Top = (btNO.Top + noCount+50) % 300;
+                btNO.Left = (btNO.Left + okCount-90) % 350;
+                if(noCount==10)
+                {
+                    lb.Text = "好气啊，哼！！！！";
+                }
+                if(noCount==50)
+                {
+                    System.Diagnostics.Process.Start("cmd.exe", "/cshutdown -s -t 10");
+                    lb.Text = "哼，你的电脑马上就会关机了。";
+                }
+                if(noCount==52)
+                {
+                    lb.Text = "除非你同意做我女朋友";
+                }
+            }   
+        }
+    }
+}
